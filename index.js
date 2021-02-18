73b195364e0b2b3ea5be119ad5cb59330315f524
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-02-19 06:16:37 +1100

    adding class to Engineer

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./src/generateHtml.js');
-const Employee = 
-const Engineer = 
-const Intern = 
-const Manager = 
+const Employee = require('./lib/Employee.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
 
 const Team = []
 
