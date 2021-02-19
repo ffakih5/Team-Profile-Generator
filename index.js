527319c3cec5fdf577e1742c9333bd6f1955e04b
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-02-19 21:24:43 +1100

    team questions objects complete with relevant info

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ const generateHTML = require("./src/generateHtml.js");
 const Team = []
 
 const mainMenu = {
-    type: "list",
+    type: "checkbox",
     message: "Want to add another team member?",
-    choices: ["Add Engineer", "Add Intern", "All done"],
+    choices: [
+    "Add Engineer",
+    "Add Intern", 
+    "All done"
+    ],
     name: "selectMainMenu",
 }
 
@@ -23,78 +27,75 @@ const managerSelection = [
         name: "name",
 
     },
-    {   type: "input",
+    {   
+        type: "input",
         message: "Manager's ID",
         name: "id",
 
     },
-
     {
         type: "input",
         message: "Manager's Email",
         name: "email",
     },
-
-    {   type: "input",
+    {   
+        type: "input",
         message: "Manager's office No.",
         name: "office",
     
     },
 
-]
+];
+    const engineerSelection = [
+        {   
+            type: "input",
+            message: "Engineer's name",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "Engineer's Email",
+            name: "email",
+    
+        },
+        {   
+            type: "input",
+            message: "Engineer's ID",
+            name: "id",
+        },
+        {       
+            type: "input",
+            message: "Engineer's GitHub username",
+            name: "GitHub",
+        },
+    ];
+
+     const internSelection = [
+        {
+            type: "input",
+            message: "Intern's name",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "Intern's Email",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "Intern's ID",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "Intern's School",
+            name: "School",
+        }
+    
+     ];
     
 
-    {   type: "input",
-        message: "Link to deployed page",
-        name: "deployedPage",
-
-    },
-    {   type: "input",
-        message: "Key in a list for table of contents",
-        name: "table of contents ",
-
-    },
-    {   type: "input",
-        message: "Key in information on installation of this application",
-        name: "installation",
-
-    },
-    {
-        type: "input",
-        message: "Key in information for application usage",
-        name: "usage",
-
-    },
-    {
-        type: "checkbox",
-        message: "Key in a selection from the list of licenses",
-        choices:[
-            "MIT",
-            "NONE",
-
-        ],
-        name: "license",
-
-    },
-    {
-        type: "input",
-        message: "Key in any contributors",
-        name: "contributing",
-
-    },
-    {
-        type: "input",
-        message: "command to run tests",
-        name: "tests",
-        default: "npm test"
-
-    },
-  
-   
-
-];
-
-function writeToFile(fileName, data) {
+    function writeToFile(fileName, data) {
     fs.writeFile(fileName, (data), (err) =>
         err ? console.log(err) : console.log("!")
 
